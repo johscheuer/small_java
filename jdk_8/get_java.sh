e913f326b757fc91d0fd12044b3e269692d40caf
Johannes Scheuermann <johannes.scheuermann@inovex.de>
2015-02-06 16:59:08 +0100

    Inital add jdk/jre 8

---
+++ jdk_8/get_java.sh
@@ -0,0 +1,9 @@
+JDK_PACKAGE=jdk-8u31-linux-x64.tar.gz
+JDK_DIR=jdk
+JDK_UNPACKED=jdk1.8.0_31
+
+if [ -d $JDK_DIR ]; then rm -rf $JDK_DIR; fi
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/$JDK_PACKAGE
+tar xfz $JDK_PACKAGE
+mv $JDK_UNPACKED $JDK_DIR
+rm -rf $JDK_PACKAGE
---
+++ jre_8/get_java.sh
@@ -0,0 +1,9 @@
+JRE_PACKAGE=jre-8u31-linux-x64.tar.gz
+JRE_DIR=jre
+JRE_UNPACKED=jre1.8.0_31
+
+if [ -d $JRE_DIR ]; then rm -rf $JRE_DIR; fi
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/$JRE_PACKAGE
+tar xfz $JRE_PACKAGE
+mv $JRE_UNPACKED $JRE_DIR
+rm -rf $JRE_PACKAGE
